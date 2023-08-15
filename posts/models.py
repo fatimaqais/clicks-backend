cb19ba8ad4b9f31afcf9d2077b6f232e2a46ce35
fatimaqais <fatima_qais@hotmail.com>
2023-08-15 14:12:28 +0000

    Add posts serializers and model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/', default='../placeholder_fjdrce', blank=True
+    )
+    caption = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} shared a post on {self.updated_at}'
