800b8d9504d51520d4408dd46c74b949d385b6f1
fatimaqais <fatima_qais@hotmail.com>
2023-08-17 21:24:29 +0000

    Add events views and urls in api folder

--- events/views.py
+++ events/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status, permissions, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Events
+from .serializers import EventsSerializer
+from clicks_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class EventsList(generics.ListCreateAPIView):
+    serializer_class = EventsSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    queryset = Events.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class EventsDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = EventsSerializer
+
+    def get_object(self, pk):
+        try:
+            event = Events.objects.get(pk=pk)
+            self.check_object_permissions(self.request, event)
+            return event
+        except Events.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventsSerializer(
+            event, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventsSerializer(
+            event, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
