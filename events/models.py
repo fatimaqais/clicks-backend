2201c24838b09870c518fe2238ea3f44d727691a
fatimaqais <fatima_qais@hotmail.com>
2023-08-17 21:21:10 +0000

    Add events model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Events(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    image = models.ImageField(
+        upload_to='images/', default='../event_cn9ezb.jpg'
+    )
+    details = models.TextField(blank=True)
+    date = models.DateField(blank=False)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
