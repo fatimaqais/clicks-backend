a5ef74d02590d30fe7cb20670e0c9428d5aca348
fatimaqais <fatima_qais@hotmail.com>
2023-09-07 14:41:39 +0000

    Create following serializer

--- following/models.py
+++ following/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Folllowing(models.Model):
+class Following(models.Model):
     owner = models.ForeignKey(
         User, related_name='following', on_delete=models.CASCADE
     )
