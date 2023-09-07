079e57db12767c1b5f80d34e850bc735cc6c1b83
fatimaqais <fatima_qais@hotmail.com>
2023-09-07 22:12:33 +0000

    Add follower details to profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,18 +1,29 @@
 from rest_framework import serializers
 from .models import Profile
+from following.models import Following
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            followers = Following.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return followers.id if followers else None
+        return None
+
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'image', 'status', 'is_owner'
+            'image', 'status', 'is_owner', 'following_id'
         ]
