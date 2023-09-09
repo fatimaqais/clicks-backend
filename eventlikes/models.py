81cffd8c042b23811a9e6bd4296a60215a6b0e48
fatimaqais <fatima_qais@hotmail.com>
2023-09-09 18:40:29 +0000

    Create a model for the likes on an event

--- eventlikes/models.py
+++ eventlikes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Events
 
-# Create your models here.
+
+class LikeEvent(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Events, related_name='likedevent', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ['owner', 'event']
+
+    def __str__(self):
+        return f'{self.owner} has liked {self.event}'
