86aa5f7d0449e76c836e5ce086467d6dee36ff7c
fatimaqais <fatima_qais@hotmail.com>
2023-09-09 00:21:09 +0000

    Create a custom model for evnt review

--- eventreview/models.py
+++ eventreview/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Events
 
-# Create your models here.
+
+class EventReviews(models.Model):
+    RATING = [
+        ('Excellent', 'Excellent'),
+        ('Good', 'Good'),
+        ('Average', 'Average'),
+        ('Bad', 'Bad'),
+        ('Horrible', 'Horrible')
+    ]
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    review = models.TextField()
+    image = models.ImageField(
+        upload_to='images/', default='../placeholder_fjdrce', blank=True
+    )
+    rating = models.CharField(
+        max_length=50, choices=RATING, default='Average'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'event']
+
+    def __str__(self):
+        return f"{self.owner}'s review"
