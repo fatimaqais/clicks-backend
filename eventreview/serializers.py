7f4d86957cf61dd6d45fabfe7a89a8fd5ac16b77
fatimaqais <fatima_qais@hotmail.com>
2023-09-09 00:22:00 +0000

    Create views and serializers for reviews

---
+++ eventreview/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import EventReviews
+from django.db import IntegrityError
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = EventReviews
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'review', 'rating', 'image', 'event'
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'A review for this event has already been created.'
+            })
+
+
+class ReviewDetailSerializer(ReviewSerializer):
+    event = serializers.ReadOnlyField(source='event.id')
--- eventreview/views.py
+++ eventreview/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from clicks_api.permissions import IsOwnerOrReadOnly
+from .models import EventReviews
+from .serializers import ReviewSerializer, ReviewDetailSerializer
 
-# Create your views here.
+
+class ReviewList(generics.ListCreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = EventReviews.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReviewDetailSerializer
+    queryset = EventReviews.objects.all()
