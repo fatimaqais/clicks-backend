9089673ccb5efd913ebf742f351fbe2443d8d287
fatimaqais <fatima_qais@hotmail.com>
2023-10-23 11:20:31 +0000

    add filter to the event reviews

--- eventreview/views.py
+++ eventreview/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from clicks_api.permissions import IsOwnerOrReadOnly
 from .models import EventReviews
 from .serializers import ReviewSerializer, ReviewDetailSerializer
@@ -8,6 +9,8 @@ class ReviewList(generics.ListCreateAPIView):
     serializer_class = ReviewSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = EventReviews.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['event']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
