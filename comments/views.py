4516b102b5d261b6513b361484eabd96ccc699b5
fatimaqais <fatima_qais@hotmail.com>
2023-10-23 10:49:04 +0000

    Add filter to comments

--- comments/views.py
+++ comments/views.py
@@ -9,7 +9,6 @@ class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
-
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['post']
 
